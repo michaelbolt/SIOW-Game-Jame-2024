38afc7a70a01ca70e763de9eb1de729885f94477
Michael Bolt <boltmichaelt@gmail.com>
2024-06-10 07:49:05 +0200

    start of PlayerController script
    Created PlayerController script and added Gizmo to show the step-offset (in case we ever want to change it)

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private InputActions inputActions;
+    private CharacterController characterController;
+
+    // Awake is called when the script is first loaded
+    private void Awake()
+    {
+        inputActions = new InputActions();
+        characterController = GetComponent<CharacterController>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region GIZMOS
+    /// <summary>
+    /// Draws Gizmos in Scene view for debugging
+    /// </summary>
+    private void OnDrawGizmos()
+    {
+        DrawStepHeightPlane();
+    }
+
+    private void DrawStepHeightPlane()
+    {
+        if (characterController != null)
+        {
+            Gizmos.color = new Color(1, 0, 0, 0.3f);
+            Gizmos.DrawCube(
+                transform.position                                      // Player location (bottom-center of model
+                + characterController.center                            // + CharacterController center point -> center of CharacterController mesh
+                - new Vector3(0, characterController.height / 2, 0)     // - half of CharacterController height -> bottom of CharacterController mesh
+                + new Vector3(0, characterController.stepOffset, 0)     // + stepOffset -> height of step we can "move up" without interaction
+                + new Vector3(0, 0.01f / 2, 0),                         // + half of Gizmo cube height -> bottom of gizmo is aligned with stepOffset
+                new Vector3(
+                    2 * characterController.radius, 
+                    0.01f, 
+                    2 * characterController.radius
+                )
+                );
+        }
+    }
+
+    #endregion
+}
