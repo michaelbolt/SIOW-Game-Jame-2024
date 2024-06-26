c3f4d3a24ae9b340a66355d4128529ea157b8550
Michael Bolt <boltmichaelt@gmail.com>
2024-06-26 08:00:47 +0200

    PLAYER CONTROLLER - refactored horizontal movement
    Moved player horizontal movement into an isolated class to encapsulate the behavior and let the Playercontroller Monobehvior worry only about providing Unity hooks

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,38 +4,6 @@ using UnityEngine;
 [RequireComponent(typeof(Vector2GameEventListener), typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-
-    #region UNITY_LIFECYCLE
-    // required Components
-    private Camera myCamera;
-    private CharacterController characterController;
-
-    /// <summary>Gets the <c>CharacterController</c> Component when instance is loaded.</summary>
-    private void Awake() { characterController = GetComponent<CharacterController>(); }
-
-    /// <summary>Get Scene Main camera before first frame is drawn.</summary>
-    private void Start() { myCamera = Camera.main; }
-
-
-    /// <summary>Applies motion on every Physics update.</summary>
-    private void FixedUpdate() { ApplyMotion();}
-    #endregion
-
-    #region PLAYER_INPUT
-    private Vector2 moveInput = Vector2.zero;
-    private float moveAngle = 0.0f;
-    private float moveMagnitude = 0.0f;
-
-    /// <summary>Callback to be triggered when player <c>move</c> input changes</summary>
-    public void OnMoveEvent(Vector2 playerInput)
-    {
-        moveInput = playerInput;
-        moveAngle = -Vector2.SignedAngle(Vector2.up, moveInput);
-        moveMagnitude = moveInput.magnitude;
-    }
-    #endregion
-
-    #region PLAYER_MOVEMENT
     [Header("Player Movement")]
 
     [Tooltip("Walking speed of character (units / sec)")]
@@ -51,85 +19,36 @@ public class PlayerController : MonoBehaviour
     public float gravity = -0.3f;
 
     private float verticalVelocity = 0.0f;
-    private float horizontalSpeed = 0.0f;
-    private float horizontalAcceleration = 0.0f;
+
+    private PlayerHorizontalMotion horizontalMotion;
+    private CharacterController characterController;
 
 
-    /// <summary>
-    /// Apply motion to the GameObject based on player input, the current camera 
-    /// orientation, and gravity.
-    /// </summary>
-    /// <remarks>
-    /// <list type="bullet">
-    /// <item>
-    /// Motion is performed by first rotating the object towards the direction of player 
-    /// input (relative to the current camera view) <em>then</em> moving along that 
-    /// direction.
-    /// </item>
-    /// <item>
-    /// Rotation is achieved by using a spherical linear-interpolation (SLERP) between 
-    /// the current object orientation Quaternion and the "desired direction of travel"
-    /// Quaternion.
-    /// </item>
-    /// <item>
-    /// Forward motion is achieved using a smooth-damping function to approach the 
-    /// maximum walking speed scaled by the player's input.
-    /// </item>
-    /// </list>
-    /// </remarks>
-    private void ApplyMotion()
+    // called when the Script instance is being loaded
+    private void Awake() { characterController = GetComponent<CharacterController>(); }
+
+
+    // called before Update is called for the first time
+    void Start() { horizontalMotion = new PlayerHorizontalMotion(Camera.main); }
+
+    /// <summary>Callback to be triggered when player <c>move</c> input changes</summary>
+    public void OnMoveEvent(Vector2 playerInput) { horizontalMotion.UpdatePlayerInput(playerInput); }
+
+
+    // called on Physics updates
+    private void FixedUpdate()
     {
-        if (characterController != null)
-        {
-            if (moveMagnitude > 0)
-            {
-                Vector3 directionOfPlayerInput = Quaternion.AngleAxis(moveAngle, Vector3.up) * MyUtils.ProjectForwardOntoXZPlane(myCamera.transform);
-                Quaternion orientationOfPlayerInput = Quaternion.LookRotation(directionOfPlayerInput, Vector3.up);
-                transform.rotation = Quaternion.Slerp(transform.rotation, orientationOfPlayerInput, rotationRatio);
+        if (characterController == null) { return; }
 
-                horizontalSpeed = Mathf.SmoothDamp(horizontalSpeed, walkSpeed * moveMagnitude, ref horizontalAcceleration, speedUpTime);
-            }
-            else
-            {
-                horizontalSpeed = Mathf.SmoothDamp(horizontalSpeed, 0.0f, ref horizontalAcceleration, stopTime);
-            }
-            characterController.Move(horizontalSpeed * Time.deltaTime * transform.forward);
+        // perform horizontal motion
+        horizontalMotion.RotateTowardsPlayerInput(transform, rotationRatio);
+        float speed = horizontalMotion.UpdateSpeed(walkSpeed, speedUpTime, stopTime);
+        characterController.Move(speed * Time.deltaTime * transform.forward);
 
-            /* Always apply gravity, regardless of player input */
-            if (characterController.isGrounded && verticalVelocity < 0.0f) verticalVelocity = 0.0f;
-            else verticalVelocity += gravity;
-            characterController.Move(verticalVelocity * Time.deltaTime * Vector3.up );
-        }
-    }
-    #endregion
-
-    #region GIZMOS
-    /// <summary>
-    /// Draws Gizmos in Scene view for debugging
-    /// </summary>
-    private void OnDrawGizmos()
-    {
-        DrawStepHeightPlane();
+        /* Always apply gravity, regardless of player input */
+        if (characterController.isGrounded && verticalVelocity < 0.0f) verticalVelocity = 0.0f;
+        else verticalVelocity += gravity;
+        characterController.Move(verticalVelocity * Time.deltaTime * Vector3.up);
     }
 
-    private void DrawStepHeightPlane()
-    {
-        if (characterController != null)
-        {
-            Gizmos.color = new Color(1, 0, 0, 0.3f);
-            Gizmos.DrawCube(
-                transform.position                                      // Player location (bottom-center of model
-                + characterController.center                            // + CharacterController center point -> center of CharacterController mesh
-                - new Vector3(0, characterController.height / 2, 0)     // - half of CharacterController height -> bottom of CharacterController mesh
-                + new Vector3(0, characterController.stepOffset, 0)     // + stepOffset -> height of step we can "move up" without interaction
-                + new Vector3(0, 0.01f / 2, 0),                         // + half of Gizmo cube height -> bottom of gizmo is aligned with stepOffset
-                new Vector3(
-                    2 * characterController.radius,
-                    0.01f,
-                    2 * characterController.radius
-                )
-                );
-        }
-    }
-    #endregion
 }
---
+++ Assets/Scripts/Player/PlayerHorizontalMotion.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHorizontalMotion
+{
+    private readonly Camera mainCamera;
+
+    private float moveAngle = 0.0f;
+    private float moveMagnitude = 0.0f;
+
+    private float currentSpeed = 0.0f;
+    private float currentAcceleration = 0.0f;
+
+    /// <summary>Construct a new <c>PlayerHorizontalMotion</c> object.</summary>
+    /// <param name="mainCamera"><c>Camera</c> to base movement relative to.</param>
+    public PlayerHorizontalMotion(Camera mainCamera) 
+    { 
+        this.mainCamera = mainCamera;
+    }
+
+    /// <summary>Update player input for movement control.</summary>
+    public void UpdatePlayerInput(Vector2 playerInput)
+    {
+        moveAngle = -Vector2.SignedAngle(Vector2.up, playerInput);
+        moveMagnitude = playerInput.magnitude;
+    }
+
+    /// <summary>
+    /// Rotate the provided <c>Transform</c> towards the direction of player input 
+    /// (relative to the <c>mainCamera</c>) by a given amount.
+    /// </summary>
+    /// <param name="objTransform">Object to rotate towards player input direction</param>
+    /// <param name="by">Amount to rotate by [0, 1]</param>
+    public void RotateTowardsPlayerInput(Transform objTransform, float by)
+    {
+        if (moveMagnitude == 0.0f) { return;  }  // do not rotate if no player input
+        
+        Vector3 cameraForward_xz = ProjectCameraForwardOntoXZPlane();
+        Vector3 playerInputDirection = Quaternion.AngleAxis(moveAngle, Vector3.up) * cameraForward_xz;
+        Quaternion targetOrientation = Quaternion.LookRotation(playerInputDirection, Vector3.up);
+
+        objTransform.rotation = Quaternion.Slerp(objTransform.rotation, targetOrientation, by);
+    }
+
+    /// <summary>Smoothly update the current speed.</summary>
+    /// <param name="maxSpeed">Max speed to accelerate towards; will be scaled by player input.</param>
+    /// <param name="accelerationTime">Time to spend accelerating to <c>maxSpeed</c>.</param>
+    /// <param name="stopTime">Time to spend decelerating if no player input.</param>
+    /// <returns></returns>
+    public float UpdateSpeed(float maxSpeed, float accelerationTime, float stopTime)
+    {
+        currentSpeed = Mathf.SmoothDamp(
+            currentSpeed,
+            maxSpeed * moveMagnitude,
+            ref currentAcceleration,
+            moveMagnitude == 0.0f ? stopTime : accelerationTime
+        );
+        return currentSpeed;
+    }
+
+
+    /// <summary>Returns the <c>mainCamera.forward</c> vector transformed onto the World-space X-Z plane.</summary>
+    private Vector3 ProjectCameraForwardOntoXZPlane()
+    {
+        Vector3 worldSpaceForward = mainCamera.transform.TransformDirection(Vector3.forward);
+        worldSpaceForward.y = 0;  // clear y (up/down) component
+        worldSpaceForward.Normalize();  // normalize onto x-z plane
+        return worldSpaceForward;
+    }
+}
