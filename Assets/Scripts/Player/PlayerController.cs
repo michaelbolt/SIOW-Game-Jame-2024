8d10ef5283283f17a46de57718ce9acdbfda0f5d
Michael Bolt <boltmichaelt@gmail.com>
2024-06-29 10:45:38 +0200

    GHOST FLOAT - working Ghost Float ability
    moved the vertical motion component of the player character into a separate class and implemented a jumping state-machine

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [RequireComponent(typeof(Vector2GameEventListener), typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    [Header("Player Movement")]
+    [Header("Horizontal Movement")]
 
     [Tooltip("Walking speed of character (units / sec)")]
     public float walkSpeed = 5.0f;
@@ -15,12 +15,19 @@ public class PlayerController : MonoBehaviour
     // TODO: IMPLEMENT THIS IN TERMS OF TIME BY SCALING WITH TIME.DELTATIME
     [Tooltip("Portion of the rotation between current and desired orientation to apply each physics update"), Range(0, 1.0f)]
     public float rotationRatio = 0.2f;
-    [Tooltip("Gravitational Constant (units / sec / sec)")]
-    public float gravity = -0.3f;
 
-    private float verticalVelocity = 0.0f;
+    [Header("Vertical Movement")]
+    [Tooltip("Gravitational Constant (units / sec / sec)")]
+    public float gravity = -15;
+    [Tooltip("How high to hover at")]
+    public float hoverHeight = 0.50f;
+    [Tooltip("How long jumping to Hover Height will take")]
+    public float hoverRiseTime = 0.25f;
+    [Tooltip("How long to hover before landing")]
+    public float maxHoverTime = 3.0f;
 
     private PlayerHorizontalMotion horizontalMotion;
+    private PlayerVerticalMotion verticalMotion;
     private CharacterController characterController;
 
 
@@ -29,10 +36,16 @@ public class PlayerController : MonoBehaviour
 
 
     // called before Update is called for the first time
-    void Start() { horizontalMotion = new PlayerHorizontalMotion(Camera.main); }
+    void Start() { 
+        horizontalMotion = new PlayerHorizontalMotion(Camera.main);
+        verticalMotion = new PlayerVerticalMotion();
+    }
+
 
     /// <summary>Callback to be triggered when player <c>move</c> input changes</summary>
     public void OnMoveEvent(Vector2 playerInput) { horizontalMotion.UpdatePlayerInput(playerInput); }
+
+    public void OnJumpEvent(bool playerInput) { verticalMotion.UpdateJumpPressed(playerInput); }
 
 
     // called on Physics updates
@@ -40,15 +53,16 @@ public class PlayerController : MonoBehaviour
     {
         if (characterController == null) { return; }
 
-        // perform horizontal motion
+        // rotate towards direction of input
         horizontalMotion.RotateTowardsPlayerInput(transform, rotationRatio);
-        float speed = horizontalMotion.UpdateSpeed(walkSpeed, speedUpTime, stopTime);
-        characterController.Move(speed * Time.deltaTime * transform.forward);
-
-        /* Always apply gravity, regardless of player input */
-        if (characterController.isGrounded && verticalVelocity < 0.0f) verticalVelocity = 0.0f;
-        else verticalVelocity += gravity;
-        characterController.Move(verticalVelocity * Time.deltaTime * Vector3.up);
+        
+        // move character
+        // NOTE: must make _one_ call to `characterController.Move()` so that `characterController.isGrounded` will work reliably
+        float horizontalSpeed = horizontalMotion.UpdateSpeed(walkSpeed, speedUpTime, stopTime);
+        float verticalVelocity = verticalMotion.UpdateVelocity(characterController.isGrounded, gravity, hoverHeight, hoverRiseTime, maxHoverTime);
+        characterController.Move(
+            Time.deltaTime * (horizontalSpeed * transform.forward + verticalVelocity * Vector3.up)
+        );
     }
 
 }
